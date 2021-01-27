cb5759e79ea404716c8880c260070cefbeabb3bd
PiotrPaprzycki <Avalok2015>
2021-01-27 14:49:16 +0100

    Bubble second part

--- src/main/java/pl/javastart/task/BubbleSort.java
+++ src/main/java/pl/javastart/task/BubbleSort.java
@@ -1,18 +1,23 @@
 package pl.javastart.task;
 
 public class BubbleSort {
+
+
     int[] sortArray(int[] array) {
+        int swapNumber = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     int tmp = array[j + 1];
                     array[j + 1] = array[j];
                     array[j] = tmp;
+                    swapNumber += 1;
                 }
             }
         }
+        System.out.println("Swap number is: " + swapNumber);
         return array;
     }
-
 }
+
 
